d42fac9edc7515961304a24f0d95bab3a01ff672
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-04-21 14:39:57 +0700

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,29 +1,43 @@
 def format_linter_error(error: dict) -> dict:
     # write your code here
-    # The slash \ is used after "else"s to avoid "\n" errors in the long ternary operators
-    return {key : error[val] for key, val in
-            {"line" : "line_number",
-            "column" : "column_number",
-            "message" : "text",
-            "name" : "code"}.items()} | {"source" : "flake8"}
+    # The slash \ is used after "else"s
+    # #to avoid "\n" errors in the long ternary operators
+    return {key : error[val] for key, val
+            in
+            {
+                "line" : "line_number",
+                "column" : "column_number",
+                "message" : "text",
+                "name" : "code"}.items()
+            }\
+        | {"source" : "flake8"}
+
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # write your code here
-    return {"errors": {}, "path": file_path, "status": "passed"} if not errors else\
-        {"errors": [format_linter_error(error) for error in errors], "path": file_path, "status": "failed"}
-
+    return {"errors": {}, "path": file_path, "status": "passed"}\
+        if not errors else\
+        {"errors": [format_linter_error(error) for error in errors],
+         "path": file_path, "status": "failed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
-    return [{key : {
-        "errors": [],
-        "path": key,
-        "status": "passed"
-        } if not val else\
-            {key: {
-                "errors": [format_linter_error(error) for error in linter_report[key]],
+    return [
+        {
+            key : {
+                "errors": [],
                 "path": key,
-                "status": "failed"
-            }}
-             for key, val in linter_report.items()}]
+                "status": "passed"
+            } if not val else {
+                key: {
+                    "errors": [
+                        format_linter_error(error)
+                        for error in linter_report[key]],
+                    "path": key,
+                    "status": "failed"
+                }
+            }
+            for key, val in linter_report.items()
+        }
+    ]
